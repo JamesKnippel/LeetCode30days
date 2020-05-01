1d0526e0ff3af707548f6104a6c9930eb5363e83
JamesKnippel <Jimmyknippel@gmail.com>
2020-05-01 13:16:26 -0700

    added further explanation from LC to MaxSquare

--- week4/MaximalSquare.js
+++ week4/MaximalSquare.js
@@ -52,3 +52,46 @@ var maximalSquare = function (matrix) {
     /* return the max, squared - cus math */
     return max ** 2;
 };
+
+
+
+/* some1 else's xplanation:
+explanation:
+
+we originally create a DP matrix, same dimensions as input matrix.
+then, we fill any number in first row, or first column, with the original number.
+each cell, we take the min of the top left, left, and top, + 1.
+then, each time we see a 1, we update the max
+at the end, we return a square of that number.
+This is pretty straight forward, but I will explain what's happening by setting DP to be min of topleft, left, and top + 1.
+
+Say we are given this:
+
+1 1 1
+1 1 1
+1 1 1
+first row stays same according to algo, then we're at (1,0). this stays same because it's in first column.
+When we get to (1,1), we take min of top left, left, and top + 1, which is 2, so DP now becomes
+
+1 1 1
+1 2 1
+1 1 1
+And we set new max to be 2.
+
+After updating more values, DP becomes:
+
+1 1 1
+1 2 2
+1 2 1
+Then, when we get to last cell, (2,2), we take min again and it becomes:
+
+1 1 1
+1 2 2
+1 2 3
+and new max is 3.
+
+Result becomes 3^2 which is 9, which is answer for this example.
+
+enjoy.
+
+*/

1d5bd403b79e5e0d04676ff89cf6b82bf828f9c3
JamesKnippel <Jimmyknippel@gmail.com>
2020-05-01 13:10:18 -0700

    maximal square

--- week4/MaximalSquare.js
+++ week4/MaximalSquare.js
@@ -1,16 +1,47 @@
-let name = Symbol('Mike');
-console.log(name);
+/* 
+Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
 
-let map = new Map();
+Example:
 
-map.set(name, 17);
+Input:
 
-map.set(Symbol('Mike'), 16);
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
 
-console.log(map);
+Output: 4
+*/
+var maximalSquare = function (matrix) {
+    let max = 0;
 
+    const grid = [...Array(matrix.length),].map(col => Array(matrix[0].length));
 
-map.set('Jim', 7);
-map.set('Jim', 8)
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[0].length; col++) {
+            if (col === 0 || row === 0) {
+                if (matrix[row][col] === '1') {
 
-console.log(map)
+                    grid[row][col] = '1';
+                    max = Math.max(max, grid[row][col]);
+
+                } else {
+                    grid[row][col] = 0;
+                }
+
+            } else {
+                if (matrix[row][col] === '1') {
+
+                    grid[row][col] =
+                        Math.min(grid[row][col - 1], grid[row - 1][col], grid[row - 1][col - 1]) + 1;
+                    max = Math.max(max, grid[row][col]);
+
+                } else {
+
+                    grid[row][col] = 0;
+                }
+            }
+        }
+    }
+    return max ** 2;
+};
